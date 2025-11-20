414b10cc83f244e6f101fa66ae24f86fe1a226a0
Andrea Bedini <andrea@bedini.au>
2025-11-20 13:29:09 +0800

    feat: add SmoothTrack iOS head tracker plugin
    • Introduce new tracker-smoothtrack module enabling iOS device head tracking via usbmuxd over USB connection
    • Implement SmoothTrack protocol integration with configurable port and per-axis rotation offsets
    • Provide settings UI for port configuration and axis offset control
    
    ci: add libusbmuxd-dev dependency for SmoothTrack support
    
    - macOS: Add libusbmuxd to brew install
    - Windows: Install libusbmuxd via vcpkg and configure CMake to use vcpkg toolchain
    - linux: Add libusbmuxd-dev to apt-get install

---
+++ tracker-smoothtrack/lang/de_DE.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>UISmoothTrackControls</name>
+    <message>
+        <source>SmoothTrack iOS settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connect your iOS device via USB and ensure SmoothTrack app is running.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>iOS Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add to axis</source>
+        <translation type="unfinished">Zur Achse hinzufügen</translation>
+    </message>
+    <message>
+        <source>yaw</source>
+        <translation type="unfinished">Gieren</translation>
+    </message>
+    <message>
+        <source>0</source>
+        <translation type="unfinished">0</translation>
+    </message>
+    <message>
+        <source>+90</source>
+        <translation type="unfinished">+90</translation>
+    </message>
+    <message>
+        <source>-90</source>
+        <translation type="unfinished">-90</translation>
+    </message>
+    <message>
+        <source>+180</source>
+        <translation type="unfinished">+180</translation>
+    </message>
+    <message>
+        <source>-180</source>
+        <translation type="unfinished">-180</translation>
+    </message>
+    <message>
+        <source>pitch</source>
+        <translation type="unfinished">Nicken</translation>
+    </message>
+    <message>
+        <source>roll</source>
+        <translation type="unfinished">Rollen</translation>
+    </message>
+</context>
+<context>
+    <name>smoothtrack</name>
+    <message>
+        <source>Can&apos;t connect to iOS device. Make sure:
+1. SmoothTrack app is running on your iOS device
+2. Device is connected via USB
+3. usbmuxd is running
+4. Port number matches SmoothTrack settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>smoothtrack_metadata</name>
+    <message>
+        <source>SmoothTrack iOS</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ tracker-smoothtrack/lang/nl_NL.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>UISmoothTrackControls</name>
+    <message>
+        <source>SmoothTrack iOS settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Connect your iOS device via USB and ensure SmoothTrack app is running.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>iOS Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add to axis</source>
+        <translation type="unfinished">Aan as toevoegen</translation>
+    </message>
+    <message>
+        <source>yaw</source>
+        <translation type="unfinished">yaw</translation>
+    </message>
+    <message>
+        <source>0</source>
+        <translation type="unfinished">0</translation>
+    </message>
+    <message>
+        <source>+90</source>
+        <translation type="unfinished">+90</translation>
+    </message>
+    <message>
+        <source>-90</source>
+        <translation type="unfinished">-90</translation>
+    </message>
+    <message>
+        <source>+180</source>
+        <translation type="unfinished">+180</translation>
+    </message>
+    <message>
+        <source>-180</source>
+        <translation type="unfinished">-180</translation>
+    </message>
+    <message>
+        <source>pitch</source>
+        <translation type="unfinished">pitch</translation>
+    </message>
+    <message>
+        <source>roll</source>
+        <translation type="unfinished">rol</translation>
+    </message>
+</context>
+<context>
+    <name>smoothtrack</name>
+    <message>
+        <source>Can&apos;t connect to iOS device. Make sure:
+1. SmoothTrack app is running on your iOS device
+2. Device is connected via USB
+3. usbmuxd is running
+4. Port number matches SmoothTrack settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>smoothtrack_metadata</name>
+    <message>
+        <source>SmoothTrack iOS</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
